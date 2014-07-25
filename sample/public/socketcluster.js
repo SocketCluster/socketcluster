566b6b2d8e011b6ea3f9960c16e4992f18987def
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2014-07-25 23:42:06 +1000

    Updated SC client in sample app

--- sample/public/socketcluster.js
+++ sample/public/socketcluster.js
@@ -4079,7 +4079,7 @@ var SCSocket = function (options) {
     self._emitBuffer = [];
     
     // Exponential backoff reconnect
-    if (!self.connected) {
+    if (!self.connected && self.options.autoReconnect) {
       var exponent = ++self._connectAttempts;
       if (exponent > 5) {
         exponent = 5;
