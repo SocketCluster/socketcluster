74913d3c46ca68997138f4f62048f27c99de157f
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2014-08-07 00:04:24 +1000

    Changed sample app default appName

--- sample/server.js
+++ sample/server.js
@@ -5,7 +5,7 @@ var socketCluster = new SocketCluster({
   workers: 1,
   stores: 1,
   port: 8000,
-  appName: 'myapp',
+  appName: 'app',
   workerController: __dirname + '/worker.js',
   balancerController: __dirname + '/balancer.js',
   storeController: __dirname + '/store.js',
