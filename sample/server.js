710dd47b0a5b70eda2bafb744b044be285ce9273
jondubois <grosjona@yahoo.com.au>
2014-10-25 19:48:52 +1100

    Sample boilerplate app now lets you specify common options via command line arguments

--- sample/server.js
+++ sample/server.js
@@ -1,11 +1,12 @@
+var argv = require('minimist')(process.argv.slice(2));
 var SocketCluster = require('socketcluster').SocketCluster;
 
 var socketCluster = new SocketCluster({
-  balancers: 1,
-  workers: 1,
-  stores: 1,
-  port: 8000,
-  appName: 'app',
+  balancers: Number(argv.b) || 1,
+  workers: Number(argv.w) || 1,
+  stores: Number(argv.s) || 1,
+  port: Number(argv.p) || 8000,
+  appName: argv.n || 'app',
   workerController: __dirname + '/worker.js',
   balancerController: __dirname + '/balancer.js',
   storeController: __dirname + '/store.js',
