135d31b2abb56685b454c6be00d819e7705ed02f
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2015-02-12 00:51:20 +1100

    Sample app works again

--- sample/worker.js
+++ sample/worker.js
@@ -15,7 +15,7 @@ module.exports.run = function (worker) {
   
   app.use(serveStatic(path.resolve(__dirname, 'public')));
 
-  httpServer.on('req', app);
+  httpServer.on('request', app);
 
   var count = 0;
 
@@ -28,6 +28,16 @@ module.exports.run = function (worker) {
       count++;
       console.log('PING', data);
       scServer.global.publish('pong', count);
+    });
+    
+    var interval = setInterval(function () {
+      socket.emit('rand', {
+        rand: Math.floor(Math.random() * 5)
+      });
+    }, 1000);
+    
+    socket.on('disconnect', function () {
+      clearInterval(interval);
     });
   });
 };
