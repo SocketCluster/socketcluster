b53c26e50768a54abef0a75bf5b7bd2148927781
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2015-03-04 00:39:27 +1100

    v2.0.2

--- sample/server.js
+++ sample/server.js
@@ -2,13 +2,11 @@ var argv = require('minimist')(process.argv.slice(2));
 var SocketCluster = require('sc2').SocketCluster;
 
 var socketCluster = new SocketCluster({
-  balancers: Number(argv.b) || 1,
   workers: Number(argv.w) || 1,
   stores: Number(argv.s) || 1,
   port: Number(argv.p) || 8000,
   appName: argv.n || 'app',
   workerController: __dirname + '/worker.js',
-  balancerController: __dirname + '/balancer.js',
   storeController: __dirname + '/store.js',
   socketChannelLimit: 100,
   rebootWorkerOnCrash: !argv.debug
