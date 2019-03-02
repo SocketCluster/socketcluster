e8bedbf126e1181b4fcf1d0544387e4bedfd9168
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-03-02 20:27:23 +0100

    v5.3.2

--- app/public/asyngular-client.js
+++ app/public/asyngular-client.js
@@ -1,5 +1,5 @@
 /**
- * Asyngular JavaScript client v5.3.1
+ * Asyngular JavaScript client v5.3.2
  */
  (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.asyngularClient = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (global){
@@ -2238,7 +2238,7 @@ class AGChannel extends ConsumableStream {
   }
 
   killOutput() {
-    this.channelKillOutput(this.name);
+    this.client.channelKillOutput(this.name);
   }
 
   killListener(eventName) {
@@ -8217,7 +8217,7 @@ module.exports = WritableConsumableStream;
 },{"./consumer":31,"consumable-stream":13}],"asyngular-client":[function(require,module,exports){
 const AGClientSocket = require('./lib/clientsocket');
 const factory = require('./lib/factory');
-const version = '5.3.1';
+const version = '5.3.2';
 
 module.exports.factory = factory;
 module.exports.AGClientSocket = AGClientSocket;
