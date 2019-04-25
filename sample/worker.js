e8ae7ec6f6bcd504d2f54d01908ac16a23866571
Mega <Aza.Noriega@gmail.com>
2019-04-26 00:37:03 +0500

    improve sample logic and comment it out

--- sample/worker.js
+++ sample/worker.js
@@ -16,43 +16,44 @@ class Worker extends SCWorker {
     var scServer = this.scServer;
 
     if (environment === 'dev') {
-      // Log every HTTP request. See https://github.com/expressjs/morgan for other
-      // available formats.
+      // Log every HTTP request.
+      // See https://github.com/expressjs/morgan for other available formats.
       app.use(morgan('dev'));
     }
     app.use(serveStatic(path.resolve(__dirname, 'public')));
 
-    // Add GET /health-check express route
+    // Listen for HTTP GET "/health-check".
     healthChecker.attach(this, app);
 
     httpServer.on('request', app);
 
-    // var count = 0;
+    /**
+     * NOTE: Be sure to replace the following sample logic with your own logic.
+     */
 
-    /*
-      In here we handle our incoming realtime connections and listen for events.
+    /**
+    var count = 0;
+    // Handle incoming websocket connections and listen for events.
+    scServer.on('connection', function (socket) {
+
+      socket.on('sampleClientEvent', function (data) {
+        count++;
+        console.log('Handled sampleClientEvent', data);
+        scServer.exchange.publish('sample', count);
+      });
+
+      var interval = setInterval(function () {
+        socket.emit('random', {
+          number: Math.floor(Math.random() * 5)
+        });
+      }, 1000);
+
+      socket.on('disconnect', function () {
+        clearInterval(interval);
+      });
+
+    });
     */
-    // scServer.on('connection', function (socket) {
-
-    // Some sample logic to show how to handle client events,
-    // replace this with your own logic
-
-    //   socket.on('sampleClientEvent', function (data) {
-    //     count++;
-    //     console.log('Handled sampleClientEvent', data);
-    //     scServer.exchange.publish('sample', count);
-    //   });
-
-    //   var interval = setInterval(function () {
-    //     socket.emit('random', {
-    //       number: Math.floor(Math.random() * 5)
-    //     });
-    //   }, 1000);
-
-    //   socket.on('disconnect', function () {
-    //     clearInterval(interval);
-    //   });
-    // });
   }
 }
 
