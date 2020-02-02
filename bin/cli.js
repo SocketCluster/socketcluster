cbe544e896fbe734366a9641127b851673ff4d53
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2020-02-02 18:32:46 +0100

    v15.0.6 - Bump scc-broker-client

--- bin/cli.js
+++ bin/cli.js
@@ -425,7 +425,7 @@ if (command === 'create') {
   } catch (e) {}
 
   let dockerCommand = `docker run -d -p ${portNumber}:8000 -v ${absoluteAppPath}:/usr/src/app/ ` +
-    `${envFlagString}--name ${appName} socketcluster/socketcluster:v15.0.5`;
+    `${envFlagString}--name ${appName} socketcluster/socketcluster:v15.0.6`;
   try {
     execSync(dockerCommand, {stdio: 'inherit'});
     successMessage(`App "${appName}" is running at http://localhost:${portNumber}`);
