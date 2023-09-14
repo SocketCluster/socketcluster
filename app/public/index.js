4e041fe54e973271d24e8ea5d3009b606bc8ea61
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2023-09-14 21:48:02 +1000

    v18.0.0

---
+++ app/public/index.js
@@ -0,0 +1,16 @@
+import { create } from '/socketcluster-client.js';
+
+// Initiate the connection to the server
+let socket = create();
+
+(async () => {
+  for await (let {error} of socket.listener('error')) {
+    console.error(error);
+  }
+})();
+
+(async () => {
+  for await (let event of socket.listener('connect')) {
+    console.log('Socket is connected');
+  }
+})();
